9d4c39db66aeac806595af2dcf2304c387f6ad37
Levente Csik <levente.b.csik@edu.bme.hu>
2020-10-07 19:13:22 +0200

    Creating backend for webapp

---
+++ Data/Migrations/20201005105301_AdvertisementsAndCities.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NgOtthonbazar.Data.Migrations
+{
+    public partial class AdvertisementsAndCities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
